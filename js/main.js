7c09d3361cf5fbf9049abcf6a70e1005f6b7862b
caraclarke <cara.clarke8@gmail.com>
2015-09-28 15:28:03 -0400

    Same rank for same score
    If players have same score they are ranked the same.

--- js/main.js
+++ js/main.js
@@ -7,25 +7,28 @@ $(document).ready(function() {
     return b[1] - a[1];
   } // end playerOrder
 
-  var printOrder = function(leagueRank) {
+  var printOrder = function(arr) {
     $('#rankings').html('');
-    for (var i = 0; i < leagueRank.length; i++) {
-      if (leagueRank.length == 1) {
-        $('#rankings').append('<li>' + leagueRank + '</li>');
-      } else if (leagueRank.length > 1) {
-        $('#rankings').append('<li>' + leagueRank[i] + '</li>');
-      }
-    }
-  }
-
-  $('#add').on('click', function() {
     leagueInput = $('#nameInput').val().split(',');
     var playerName = leagueInput[0].trim();
     var playerScore = parseInt(leagueInput[1].trim());
 
-    var sorted = leagueRank.push([playerName, playerScore]);
+    leagueRank.push([playerName, playerScore]);
+
+      var previousRank = 1;
+      for (var i = 0; i < leagueRank.length; i++) {
+        if (leagueRank[i - 1] && leagueRank[i - 1][1] == leagueRank[i][1]) {
+          $('#rankings').append('<li>' + previousRank + '. ' + arr[i] + '</li>');
+        } else {
+          previousRank = i + 1;
+          $('#rankings').append('<li>' + previousRank + '. ' + arr[i] + '</li>');
+        }
+      }
+    } // end printOrder
+
+  $('#add').on('click', function() {
     printOrder(leagueRank.sort(playerOrder));
     $('#nameInput').val('');
-  });
+  }); // end onclick
 
 }); // end document ready
