3d658250c4d66db261696c7e093cd7be1e3f7154
caraclarke <cara.clarke8@gmail.com>
2015-09-28 11:07:04 -0400

    Take input, split and assign to different values
    Value is being taken from input box, split and assigned to two variables.
    It is also being display in the proper name, score in the UL.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 'use strict';
 $(document).ready(function () {
-  var leagueInput;
+  // var leagueInput;
 
   $('#add').on('click', function() {
-    leagueInput = $('#nameInput').val();
-    console.log(leagueInput);
-    $('#rankings').append(leagueInput);
+    var leagueInput = $('#nameInput').val().split(',');
+    var playerName = leagueInput[0].trim();
+    var playerScore = leagueInput[1].trim();
+    console.log(playerName);
+    console.log(playerScore);
+    $('#rankings').append('<li>' + leagueInput + '</li>');
+    $('#nameInput').val(''); // reset input box to empty
   });
 
 }); // end document ready
