301a7a1ea45035df02ed51a79e414392be8d540c
caraclarke <cara.clarke8@gmail.com>
2015-09-28 11:55:03 -0400

    Sorting playerScore from highest to lowest
    Sorting playerScore from highest to lowest in console.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
 'use strict';
 $(document).ready(function () {
-  // var leagueInput;
+  var leagueInput;
+  var leagueRank = [];
+
+  var playerOrder = function (a, b) {
+    if (leagueRank.length > 1) {
+      return b[1] - a[1];
+    }
+  }
 
   $('#add').on('click', function() {
-    var leagueInput = $('#nameInput').val().split(',');
+    leagueInput = $('#nameInput').val().split(',');
     var playerName = leagueInput[0].trim();
     var playerScore = parseInt(leagueInput[1].trim());
-    console.log(playerName);
-    console.log(playerScore);
     $('#rankings').append('<li>' + leagueInput + '</li>');
+    leagueRank.push([playerName, playerScore]);
+    console.log(leagueRank.sort(playerOrder));
     $('#nameInput').val(''); // reset input box to empty
   });
 
 }); // end document ready
+
+// check playerScore so that we can order them properly
+
+
+
+// what needs to be happening is they need to go into an array
+// then i need to compare that playerScore to the one before it --> leagueRank[i][1]
+// i can have two different arrays maybe? ???? one for names and one for scores
