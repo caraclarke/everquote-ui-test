0f92ca3b8747e98b8b0eb1b491ab1a688bb3802c
caraclarke <cara.clarke8@gmail.com>
2015-09-28 11:11:47 -0400

    playerScore now integer

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
   $('#add').on('click', function() {
     var leagueInput = $('#nameInput').val().split(',');
     var playerName = leagueInput[0].trim();
-    var playerScore = leagueInput[1].trim();
+    var playerScore = parseInt(leagueInput[1].trim());
     console.log(playerName);
     console.log(playerScore);
     $('#rankings').append('<li>' + leagueInput + '</li>');
