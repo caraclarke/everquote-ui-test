d107652ec7a70f2e40ca3f5d9538d674d5731228
caraclarke <cara.clarke8@gmail.com>
2015-10-01 13:24:05 -0400

    Regex to test input. No longer clears ul#rankings when improper input.
    Using regex to make sure input is proper format.
    If improper input existing scores still displayed.

--- js/main.js
+++ js/main.js
@@ -7,49 +7,60 @@ $(document).ready(function() {
     return b[1] - a[1];
   } // end playerOrder
 
-  // regex check letters, numbers, not eqal to not a number, not an empty string
+  var validateInput = function() {
+    var inputRegex = /^[a-zA-Z\s]+(,\s?[0-9]+)$/;
+    leagueInput = $('#nameInput').val().split(',');
+
+    if (inputRegex.test(leagueInput)) {
+      return true;
+    } else { // if fails regex test
+      alert("Please enter input in format Name, Score.");
+    }
+  }
 
   var printOrder = function(arr) {
-    $('#rankings').html('');
     var previousRank = 0;
-    leagueInput = $('#nameInput').val().split(',');
-    var playerName = leagueInput[0].trim();
-    var playerScore = parseInt(leagueInput[1].trim());
-
     var nameIncluded = false;
+    leagueInput = $('#nameInput').val().split(',');
+    var playerName = leagueInput[0];
+    var playerScore = parseInt(leagueInput[1]);
 
-    for (var i = 0; i < leagueRank.length; i++) {
-      if (playerName == leagueRank[i][0]) {
-        leagueRank[i][1] += playerScore;
-        nameIncluded = true;
+    if (validateInput()) {
+      $('#rankings').html('');
+      playerName.trim();
+      for (var i = 0; i < leagueRank.length; i++) {
+        if (playerName == leagueRank[i][0]) {
+          leagueRank[i][1] += playerScore;
+          nameIncluded = true;
+        }
+      }
+
+      if (!nameIncluded){
+        leagueRank.push([playerName, playerScore]);
+      }
+
+      leagueRank.sort(playerOrder);
+
+      for (var i = 0; i < leagueRank.length; i++) {
+        if (leagueRank[i - 1] && leagueRank[i - 1][1] == leagueRank[i][1]) {
+          $('#rankings').append('<li>' + previousRank + '. ' + arr[i][0] + ', ' + arr[i][1] + ' pts</li>');
+        } else {
+          previousRank = i + 1;
+          $('#rankings').append('<li>' + previousRank + '. ' + arr[i][0] + ', ' + arr[i][1] + ' pts</li>');
+        }
       }
     }
-
-    if (!nameIncluded){
-      leagueRank.push([playerName, playerScore]);
-    }
-
-    leagueRank.sort(playerOrder)
-
-    for (var i = 0; i < leagueRank.length; i++) {
-      if (leagueRank[i - 1] && leagueRank[i - 1][1] == leagueRank[i][1]) {
-        $('#rankings').append('<li>' + previousRank + '. ' + arr[i][0] + ', ' + arr[i][1] + ' pts</li>');
-      } else {
-        previousRank = i + 1;
-        $('#rankings').append('<li>' + previousRank + '. ' + arr[i][0] + ', ' + arr[i][1] + ' pts</li>');
-      }
-    }
-
   } // end printOrder
 
-    $('#add').on('click', function() {
-      printOrder(leagueRank);
-      $('#nameInput').val('');
+  $('#add').on('click', function() {
+    printOrder(leagueRank);
+    $('#nameInput').val('');
   }); // end add onclick
 
-    $('#clear').on('click', function() {
-      $('#rankings').html('');
-      leagueRank = [];
+  $('#clear').on('click', function() {
+    $('#rankings').html('');
+    leagueRank = [];
   }); // end clear onclick
 
-}); // end document ready
+
+}); // end document.ready
